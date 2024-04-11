8f085404a03ab033bbc071338f1f195b1a061b5a
Bertin-Briffaz <166534795+Bertin-Briffaz@users.noreply.github.com>
2024-04-11 22:30:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  document.querySelectorAll('.carousel').forEach(carousel => {
+    const carouselInner = carousel.querySelector('.carousel-inner');
+    
+    carousel.querySelector('.prevSlide').addEventListener('click', () => {
+      carouselInner.scrollBy({ left: -300, top: 0, behavior: 'smooth' }); 
+    });
+    
+    carousel.querySelector('.nextSlide').addEventListener('click', () => {
+      carouselInner.scrollBy({ left: 300, top: 0, behavior: 'smooth' }); 
+    });
+  });
+});
